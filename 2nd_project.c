8de15228795f401823df76b45c68b5ef31a3f856
Kartik2212 <kri.kirangupta@gmail.com>
2026-07-02 18:00:03 +0530

    Create 2nd_project.c

---
+++ 2nd_project.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    long int varsha_didi = 201901145611;
+    long int shivani_didi = 562385785368;
+    long int farhan_bhaiya = 673696247853;
+    long int jeet = 572578357835;
+    long int sneha_didi= 982645890535;
+
+    printf("------------------------\n");
+    printf("|  NAME |  NUMBER      |\n");
+    printf("------------------------\n");
+    printf("|farhan |  %li|\n",farhan_bhaiya );
+    printf("|Jeet   |  %li|\n",jeet);
+    printf("|sneha  |  %li|\n",sneha_didi);
+    printf("|Shivani|  %li|\n",shivani_didi);
+    printf("|Farhan |  %li|\n",varsha_didi);
+   printf("------------------------\n");
+}   
