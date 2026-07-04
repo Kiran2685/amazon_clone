dc3be6e86a8c17094012d05e341dcadb976e95a2
Kartik2212 <kri.kirangupta@gmail.com>
2026-07-04 16:56:51 +0530

    Create practice.c

---
+++ practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+    long int Farhan_bhaiya = 1297405794;
+    long int Hariom_rajput = 8122888371;
+    long int Kartik_gupta = 7065454847;
+    long int Varsha_didi = 6443778953;
+    long int Prakash_sir = 3578285466;
+    long int Isha_didi = 5552784872;
+    long int Sneha_didi = 7725540303; 
+    
+    
+
+    printf("-----------------------------\n");
+    printf("|  NAME        |  NUMBER  su  |\n");
+    printf("-----------------------------\n");
+    printf("|Hariom_rajput |  %li|\n", Hariom_rajput);
+    printf("|Kartik_gupta  |  %li|\n",Kartik_gupta);
+    printf("|Prakash_sir   |  %li|\n",Prakash_sir);
+    printf("|Sneha_didi    |  %li|\n",Sneha_didi);
+    printf("|Farhan_bhaiya |  %li|\n",Farhan_bhaiya);
+    printf("|Isha_didi     |  %li|\n",Isha_didi);
+   printf("-----------------------------\n");
+}
